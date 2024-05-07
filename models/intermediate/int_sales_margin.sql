3718e0f8fb0bbe408eecc7bf9926bc2bb4f07d51
Cécile Bourdoncle <cboubou31@gmail.com>
2024-05-07 13:29:34 +0000

    update with join et purchase_cost

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,12 @@
-SELECT *
+SELECT
+products_id,
+date_date,
+orders_id,
+revenue,
+quantity,
+purchase_price,
+quantity*purchase_price AS purchase_cost
+
 FROM {{ ref("stg_raw__sales")}} AS sales
 LEFT JOIN {{ ref("stg_raw__product")}} AS product
     USING(products_id)
