aab14da10c7a65865940b771e38a66cf0ba5fd23
Cécile Bourdoncle <cboubou31@gmail.com>
2024-05-07 13:34:59 +0000

    update with margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,7 +5,8 @@ orders_id,
 revenue,
 quantity,
 purchase_price,
-quantity*purchase_price AS purchase_cost
+quantity*purchase_price AS purchase_cost,
+ROUND(revenue-(quantity*purchase_price),2) AS margin
 
 FROM {{ ref("stg_raw__sales")}} AS sales
 LEFT JOIN {{ ref("stg_raw__product")}} AS product
